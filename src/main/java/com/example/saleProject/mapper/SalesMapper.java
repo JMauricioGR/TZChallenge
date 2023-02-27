d268c5930c3a22766a72410dc1b9310b2ba4c3af
Jehison Galarza <jehison.galarza@booktofly.com.co>
2023-02-27 14:46:58 -0500

    Reto Backend TZ - mappers de colecciones y dtos configurados

--- src/main/java/com/example/saleProject/mapper/SalesMapper.java
+++ src/main/java/com/example/saleProject/mapper/SalesMapper.java
@@ -1,2 +1,20 @@
-package com.example.saleProject.mapper;public class SalesMapper {
+package com.example.saleProject.mapper;
+
+import com.example.saleProject.collections.Sales;
+import com.example.saleProject.dto.SalesDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class SalesMapper {
+
+    private ModelMapper mapper;
+
+    public SalesMapper(ModelMapper mapper) { this.mapper = mapper; }
+
+    public Function<Sales, SalesDTO> salesToDTO(){ return salesToDTO -> mapper.map(salesToDTO, SalesDTO.class); }
+
+    public Function<SalesDTO, Sales> dtoToSales(){ return sales -> mapper.map(sales, Sales.class); }
 }
